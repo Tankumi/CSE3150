e55ca8e95dfbfcaf022f455f0046fedd344a9c8c
Tankumi <90931079+Tankumi@users.noreply.github.com>
2025-09-08 18:48:39 +0000

    man idk

---
+++ week2/lab2/src/parser.cpp
@@ -0,0 +1,8 @@
+#include "parser.h"
+#include <iostream>
+
+namespace StringUtils{
+    void parseName(const std::string& fullName, std::string* firstName, std::string* lastName){
+        
+    }
+}
